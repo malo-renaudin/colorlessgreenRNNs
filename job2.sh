48f2b0fa095f3be41f4e353a77cd7877a4cdae89
malo-renaudin <malo.renaudin@hec.edu>
2025-04-08 18:52:46 +0200

    bash script with new configs

--- job2.sh
+++ job2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=multihead_cbr # Job name
+#SBATCH --job-name=lstm_adam # Job name
 #SBATCH --partition=gpu 
 #SBATCH --export=ALL 
 #SBATCH --cpus-per-task=6        # Number of CPU cores per task (adjust as needed)
@@ -20,8 +20,9 @@ which python
 echo "python-version $(python --version)"
 echo "CUDA_DEVICE: $CUDA_VISIBLE_DEVICES"
 conda list 
- 
-#python src/language_models/main.py --data /scratch2/mrenaudin/colorlessgreenRNNs/english_data --name test_cbr_scaled --classmodel 'CBR_RNN' --batch_size 1024 --lr 1.0 --cuda
-python src/language_models/main.py --data /scratch2/mrenaudin/colorlessgreenRNNs/english_data --name multihead_cbr --classmodel 'CBR_RNN' --batch_size 1024 --nheads 2 --lr 1.0 --cuda
-#python src/language_models/main.py --data /scratch2/mrenaudin/colorlessgreenRNNs/english_data --name check_ep_3_lstm --classmodel 'RNNModel' --batch_size 512  --lr 1.0 --cuda
-#regarder supplementary materials colorless green rnns
+#single head cbr rnn
+#python src/language_models/main.py --data /scratch2/mrenaudin/colorlessgreenRNNs/english_data --name test_cbr_scaled_adam --classmodel 'CBR_RNN' --batch_size 256 --lr 0.001 --cuda
+#multi head cbr rnn
+#python src/language_models/main.py --data /scratch2/mrenaudin/colorlessgreenRNNs/english_data --name multihead_cbr --classmodel 'CBR_RNN' --batch_size 256 --nheads 2 --lr 0.001 --cuda
+#lstm
+python src/language_models/main.py --data /scratch2/mrenaudin/colorlessgreenRNNs/english_data --name lstm_adam --classmodel 'RNNModel' --model 'LSTM' --emsize 650 --nhid 650 --batch_size 256 --lr 0.001 --cuda
