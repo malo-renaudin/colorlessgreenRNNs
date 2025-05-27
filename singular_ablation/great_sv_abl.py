f34dee7fe2ae376aea91efa7ab5a95ed3d3b71b3
malo-renaudin <malo.renaudin@hec.edu>
2025-05-27 16:50:24 +0200

    changes of the day

---
+++ singular_ablation/great_sv_abl.py
@@ -0,0 +1,314 @@
+import sys
+import os
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Go up one level from the script's directory (evaluation_notebooks)
+parent_dir = os.path.dirname(script_dir)
+# Join with 'src' to get the correct path to the src directory
+src_dir = os.path.join(parent_dir, "src")
+sys.path.append(os.path.abspath(src_dir))
+
+import torch
+from language_models.dictionary_corpus import Dictionary, Corpus, tokenize
+from language_models.model import RNNModel as lstm
+from language_models.utils import move_to_device, batchify, get_batch, repackage_hidden
+import torch.nn as nn
+import math
+from pathlib import Path
+import copy
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+from collections import defaultdict
+
+
+##########################################################################################
+# General Parameters and Necessary Files 
+##########################################################################################
+batch_size = 1024
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+data_path = "/scratch2/mrenaudin/colorlessgreenRNNs/english_data"
+dictionary = Dictionary(data_path)
+nounpp = "//scratch2/mrenaudin/colorlessgreenRNNs/NounPP/Stimuli/nounpp.txt"
+layers = [0,1]
+weight_type = 'hh'
+gates = ['cell','forget','input','output']
+ntokens = 50001
+
+
+nb_abl_per_weight = 10
+
+
+##########################################################################################
+# NounPP Dataset Class
+##########################################################################################
+
+class NounPPDataset(Dataset):
+    def __init__(self, nounpp_file, dictionary):
+        self.sentences = []
+        self.conditions = []
+        self.correct = []
+        self.wrong = []
+        self.encoded_sentences = []
+        self.encoded_correct = []
+        self.encoded_wrong = []
+        self.dictionary = dictionary
+
+        with open(nounpp_file, "r") as f:
+            for line in f:
+                line = line.split()
+                sentence = line[1:7]
+                condition = " ".join(line[7:9])
+                wrong = line[9]
+                correct = line[6]
+                encoded_sentence = [
+                    self.dictionary.word2idx.get(
+                        word, self.dictionary.word2idx.get("<unk>")
+                    )
+                    for word in sentence
+                ]
+                encoded_correct = self.dictionary.word2idx.get(
+                    correct, self.dictionary.word2idx.get("<unk>")
+                )
+                encoded_wrong = self.dictionary.word2idx.get(
+                    wrong, self.dictionary.word2idx.get("<unk>")
+                )
+
+                self.sentences.append(sentence)
+                self.conditions.append(condition)
+                self.correct.append(correct)
+                self.wrong.append(wrong)
+                self.encoded_sentences.append(encoded_sentence)
+                self.encoded_correct.append(encoded_correct)
+                self.encoded_wrong.append(encoded_wrong)
+
+    def __len__(self):
+        return len(self.sentences)
+
+    def __getitem__(self, idx):
+        return {
+            "sentence": self.sentences[idx],
+            "encoded_sentence": torch.tensor(
+                self.encoded_sentences[idx], dtype=torch.long
+            ),
+            "correct": self.correct[idx],
+            "encoded_correct": torch.tensor(
+                self.encoded_correct[idx], dtype=torch.long
+            ),
+            "wrong": self.wrong[idx],
+            "encoded_wrong": torch.tensor(self.encoded_wrong[idx], dtype=torch.long),
+            "condition": self.conditions[idx],
+        }
+
+##########################################################################################
+# Collate Function
+##########################################################################################
+
+def collate_fn(batch):
+    """Custom collate function to properly handle sentences as lists of strings."""
+    sentences = [item["sentence"] for item in batch]  # Keep lists of words as they are
+    encoded_sentences = torch.stack(
+        [item["encoded_sentence"] for item in batch]
+    )  # Stack tensors
+    encoded_correct = torch.stack([item["encoded_correct"] for item in batch])
+    encoded_wrong = torch.stack([item["encoded_wrong"] for item in batch])
+    correct = [item["correct"] for item in batch]
+    wrong = [item["wrong"] for item in batch]
+    conditions = [item["condition"] for item in batch]
+
+    return {
+        "sentence": sentences,
+        "encoded_sentence": encoded_sentences,
+        "correct": correct,
+        "encoded_correct": encoded_correct,
+        "wrong": wrong,
+        "encoded_wrong": encoded_wrong,
+        "condition": conditions,
+    }
+
+
+##########################################################################################
+# Priming
+##########################################################################################
+
+init_sentence = " ".join(
+    [
+        "In service , the aircraft was operated by a crew of five and could accommodate either 30 paratroopers , 32 <unk> and 28 sitting casualties , or 50 fully equipped troops . <eos>",
+        'He even speculated that technical classes might some day be held " for the better training of workmen in their several crafts and industries . <eos>',
+        "After the War of the Holy League in 1537 against the Ottoman Empire , a truce between Venice and the Ottomans was created in 1539 . <eos>",
+        'Moore says : " Tony and I had a good <unk> and off-screen relationship , we are two very different people , but we did share a sense of humour " . <eos>',
+        "<unk> is also the basis for online games sold through licensed lotteries . <eos>",
+    ]
+)
+
+
+def feed_input(model, hidden, w):
+    inp = torch.autograd.Variable(
+        torch.LongTensor([[dictionary.word2idx[w]]]).to(device)
+    )
+    out, hidden= model(inp, hidden)
+    return out, hidden
+
+
+def feed_sentence(model, h, sentence):
+    outs = []
+    for w in sentence:
+        out, h = feed_input(model, h, w)
+        outs.append(torch.nn.functional.log_softmax(out[0]).unsqueeze(0))
+    return outs, h 
+
+
+##########################################################################################
+# Evaluation function
+##########################################################################################
+
+
+def eval(model, test_dataloader, init_sentence):
+    condition_accuracies = defaultdict(int)
+    condition_counts = defaultdict(int)
+    correct_pred = 0
+    sentence_details = []
+
+    model.eval()
+
+    hidden = model.init_hidden(1)
+    #stack = model.init_stack(1)
+    init_out, init_h= feed_sentence(model, hidden, init_sentence.split(" "))
+    with torch.no_grad():
+        for batch in test_dataloader:
+            out = None
+            written = batch["sentence"]
+            sentence = batch["encoded_sentence"]
+            correct = batch["encoded_correct"]
+            wrong = batch["encoded_wrong"]
+            condition = batch["condition"]
+            batch_size = sentence.size(0)
+            hidden = (
+                init_h[0].expand(-1, batch_size, -1).contiguous(),
+                init_h[1].expand(-1, batch_size, -1).contiguous(),
+            )
+            # stack = (
+            #     init_stack.expand(batch_size, -1, -1).contiguous(),
+            # )
+            # stack=stack[0]
+            for w in range(sentence.shape[1] - 1):
+
+                word = torch.autograd.Variable(sentence[:, w].unsqueeze(0))
+                out, hidden = model(word, hidden)
+
+            log_probs = torch.nn.functional.log_softmax(out, dim=-1)
+            correct_log_probs = log_probs[0, torch.arange(batch_size), correct]
+            wrong_log_probs = log_probs[0, torch.arange(batch_size), wrong]
+            correct_predictions = correct_log_probs >= wrong_log_probs
+            for i in range(batch_size):
+                cond = condition[i]
+                pred = correct_predictions[i].item()
+                condition_counts[cond] += 1
+                condition_accuracies[cond] += pred
+
+                sentence_details.append(
+                    {
+                        "sentence": written[i],
+                        "condition": condition[i],
+                        "correct_log_prob": correct_log_probs[i],
+                        "wrong_log_prob": wrong_log_probs[i],
+                        "model_prefers_correct": pred,
+                    }
+                )
+    final_accuracies = {
+        cond: condition_accuracies[cond] / condition_counts[cond]
+        for cond in condition_accuracies
+    }
+
+    return final_accuracies
+
+
+##########################################################################################
+# Ablating Checkpoints SVs
+##########################################################################################
+
+
+def zero_out_singular_values(weight_matrix: torch.Tensor, n: int) -> torch.Tensor:
+    # Compute SVD
+    U, S, Vh = torch.linalg.svd(weight_matrix, full_matrices=False)
+    
+    # Zero out the n smallest singular values
+    S[n]=0
+    
+    # Reconstruct the matrix
+    modified_weight = (U * S.unsqueeze(0)) @ Vh
+    return modified_weight
+
+
+def modify_checkpoint_weight(checkpoint_path, layer, weight_type, gate, n, device):
+    # Load checkpoint
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    model_weights = checkpoint["model_state_dict"]
+    
+    # Extract original weight tensor (concatenated gates)
+    original_weight = model_weights[f'rnn.weight_{weight_type}_l{layer}']  # shape: (4*gate_dim, dim)
+    
+    # Split into gates
+    gates = dict(zip(['input', 'forget', 'cell', 'output'], original_weight.chunk(4, dim=0)))
+    
+    # Modify specified gate's weight matrix
+    modified_gate_weight = zero_out_singular_values(gates[gate], n)
+    
+    # Replace gate weight in gates dict
+    gates[gate] = modified_gate_weight
+    
+    # Re-concatenate gates into full weight matrix
+    modified_weight = torch.cat([gates[g] for g in ['input', 'forget', 'cell', 'output']], dim=0)
+    
+    # Create a copy of checkpoint to avoid modifying the original
+    new_checkpoint = copy.deepcopy(checkpoint)
+    new_checkpoint["model_state_dict"][f'rnn.weight_{weight_type}_l{layer}'] = modified_weight
+    
+    return new_checkpoint
+
+def evaluate_on_n_ablations(checkpoint_path, ntokens, layers, weight_type, gates, test_dataloader, n, init_sentence=init_sentence):
+    ablation ={}
+    for layer in layers:
+        ablation[f'layer_{layer}'] = {}
+        for gate in gates:
+            
+            ablation[f'layer_{layer}'][gate] = {}
+            
+            model = lstm('LSTM', ntokens, 650, 650, 2, 0, False).to(device)
+            with open(checkpoint_path, "rb") as f:
+                state_dict = torch.load(
+                    f, map_location="cuda" if device == "cuda" else "cpu"
+                )
+                model.load_state_dict(state_dict["model_state_dict"])
+                
+            ppl_original = eval(model, test_dataloader, init_sentence)
+            
+            ablation[f'layer_{layer}'][gate]['original']=ppl_original
+            ablation[f'layer_{layer}'][gate]['ablations'] = []
+            
+            for i in tqdm(range(n), desc=f"Layer {layer}, Gate {gate}"):
+                check = modify_checkpoint_weight(checkpoint_path, layer, weight_type, gate, i, device)
+                model = lstm('LSTM', ntokens, 650, 650, 2, 0, False).to(device)
+                model.load_state_dict(check['model_state_dict'])
+                ppl = eval(model, test_dataloader, init_sentence)
+                ablation[f'layer_{layer}'][gate]['ablations'].append(ppl)
+                
+    return ablation   
+
+##########################################################################################
+# Main Loop
+##########################################################################################
+
+test_dataset = NounPPDataset(nounpp, dictionary)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collate_fn)
+
+checkpoint_dir_str = "/scratch2/mrenaudin/colorlessgreenRNNs/checkpoints/lstm_adam_full_check_shuffled"
+checkpoint_files =  [f'epoch_1_batch_{i}.pt' for i in range(0, 301, 1)] + [f'epoch_1_batch_{i}.pt'for i in range(400, 9300, 100)]+[f'epoch_{i}.pt' for i in range(1, 41, 1)]
+checkpoint_dir = Path(checkpoint_dir_str)
+
+ablation = {}
+for item_name in checkpoint_files:
+    item_path = checkpoint_dir / item_name
+    ablation[f'{item_path}'] = evaluate_on_n_ablations(item_path, ntokens, layers, weight_type, gates, test_dataloader, nb_abl_per_weight)
+
+torch.save(ablation, '/scratch2/mrenaudin/colorlessgreenRNNs/singular_ablation/great_sv_ablations.pt')
---
+++ wm_tests/prepare_preface_and_intervening_text.py
@@ -0,0 +1,395 @@
+import numpy as np
+import string
+import os, json
+import re
+
+# ===== FUNCTIONS ===== #
+
+
+def add_sections(chunks, skip_last_item=True):
+    out = []
+
+    if len(chunks) > 1:
+        out += chunks[0]
+        for chunk in chunks[1::]:
+            if skip_last_item:
+                out += chunk[0:-1]
+            else:
+                out += chunk[0::]
+    else:
+        out += chunks[0]
+    return out
+
+
+def permute_tokens(token_string, seed=12345):
+    # set random seed
+    rng = np.random.RandomState(seed)
+
+    # lower case the first character prior to shuffling, such that it can't appear capitalized
+    # in other positions, don't do that for proper names
+    # also strip the final punctuation
+    #token_list = token_string.strip(".").split()#word_tokenize(token_string.strip("."))
+    stripped_string = token_string.strip(".")
+    # Pattern: \w+ matches one or more word characters (alphanumeric + underscore)
+    #          [^\s\w] matches a single character that is NOT whitespace and NOT a word character (i.e., punctuation)
+    token_list = re.findall(r"\w+|[^\s\w]", stripped_string)
+    first = token_list[0]
+    proper = ["Mary", "Sarah", "Ryan"]
+    if first not in proper:
+        token_list = [token_list[0].lower()] + token_list[1:]
+
+    shuf = rng.permutation(token_list)
+
+    if shuf[0] in string.punctuation:
+        pnct = shuf[0]
+        shuf[0] = shuf[1].capitalize()
+        shuf[1] = pnct
+    else:
+        shuf[0] = shuf[0].capitalize()
+
+    shuf = (
+        " ".join(shuf.tolist()).replace(" :", ":").replace(" ,", ",").replace(" .", ".")
+    )
+
+    return shuf
+
+
+def permute_sents(sent_list):
+    # set random seed
+    rng = np.random.RandomState(54321)
+
+    out_sents = []
+    for sent in sent_list:
+        shuf = permute_tokens(sent)
+        out_sents.append(shuf + ".")
+
+    return rng.permutation(out_sents).tolist()
+
+
+def replace_tokens(text, pairs_tuple):
+    for pair in pairs_tuple:
+        text = text.replace(*pair)
+
+    return text
+
+
+# define prefixes
+prefixes = {
+    "sce1": {"1": "Before the meeting, Mary wrote down the following list of words:"},
+    "sce1rnd": {
+        "1": "Before the meeting, Mary wrote down the following list of words:"
+    },
+    "sce2": {"1": "Before the meeting, Mary wrote down the following list of words:"},
+    "sce3": {
+        "1": "Before the meeting, Mary wrote down the following lists of words. One was:"
+    },
+    "sce4": {
+        "1": permute_tokens(
+            "Before the meeting, Mary wrote down the following list of words:"
+        )
+    },
+    "sce5": {"1": "Before the meeting, Mary wrote down the following list of words,"},
+    "sce6": {"1": "Before the meeting, John wrote down the following list of words:"},
+    "sce7": {"1": "Before the meeting, Mary wrote down the following list of words:"},
+}
+
+
+# define prefixes for the n-gram experiment
+prefixes_repeated_ngrams = {
+    "sce1": {"1": "In the newspaper, somebody wrote the following words:"},
+}
+
+# ===== VIGNETTES =====#
+
+# ===== MARY SCENARIO =====#
+
+a1 = ["After the meeting, she took a break and had a cup of coffee."]
+
+a2 = [
+    "After the meeting, Mary went for a walk.",
+    "It was a busy day and she needed a break.",
+    "Outside was really beautiful and warm and the flowers in the park were blooming.",
+]
+
+a3 = [
+    "While she was walking, she listened to the wonderful bird songs.",
+    "During the walk, Mary could not stop thinking about the meeting.",
+    "She was thinking about the discussions she had with her coworkers.",
+    "Luckily, she met her neighbors Sarah and Ryan and they talked briefly.",
+    "After they said goodbye, Mary started walking towards home.",
+]
+
+a4 = [
+    "The couple has just moved to the area from a different city.",
+    "Mary thought they were very a lovely couple and made good company.",
+    "They were just getting to know the neighborhood and this was their first time in the park.",
+    "Mary was curious what were their first impressions of the town.",
+    "The neighborhood felt very safe to them and they absolutely loved the park.",
+    "This was only their second time visiting the park.",
+    "There was so much to discover, so many winding paths and hidden gardens.",
+    "After they said goodbye, Mary started walking towards home.",
+]
+
+a5 = [
+    "It was not a big park by any means, but it offered a quiet refuge where one can escape the worries of everyday life.",
+    "It also offered opportunities to do sports of all kinds.",
+    "Young people from around the area played basketball, football, or volleyball.",
+    "Others took part in outdoor workout sessions.",
+    "Young families were going on a stroll with their children.",
+    "Finally, there were so many people who brought their dogs for a walk.",
+    "It was incredibly satisfying to see the joy our animal friends get when you throw them a ball.",
+    "All this diversity of people and activities made a walk in this park a truly rewarding and relaxing daily routine.",
+    "In fact, Sarah and Ryan were thinking of getting a dog.",
+    "They have not fully decided yet but they really wanted to spend more time outdoors.",
+    "Mary liked dogs as well, but she was more of a cat person herself.",
+    "She and her husband had two cats.",
+    "One was two and the other four years old.",
+    "They were very independent and spent most of their time outdoors.",
+    "Mary thought having an animal was a great idea.",
+    "They talked for a little bit and then Sarah and Ryan invited her to come over for a cup of coffee.",
+    "Mary said she had time over the weekend.",
+    "After they said goodbye, Mary started walking towards home.",
+]
+
+
+# ===== INCONGRUENT OCEAN SCENARIO ==== #
+
+b1 = ["There is a voice in the waters of the great sea. It calls to man continually."]
+
+b2 = [
+    "Sometimes it thunders in the tempest, when the waves leap high and strong and the wild winds shriek and roar.",
+    "Sometimes it whispers in the calm, small voice, as if to solicit our regard.",
+]
+
+b3 = [
+    "The sea has much to say; far more than could possibly be comprehended in one volume, however large.",
+    "It tells us of the doings of man on its broad bosom, from the day in which he first ventured to paddle along shore "
+    "to the day when he launched his great iron ship, "
+    "and rushed out to sea.",
+]
+
+b4 = [
+    "Before proceeding to the consideration of the wonders connected with and contained in the sea, "
+    "we shall treat of the composition of the sea itself and of its extent, depth, and bottom.",
+    "What is the sea made of?",
+    "Salt water, is the ready reply that rises naturally to every lip.",
+    "But to this we add the question, what is salt water?",
+    "To these queries we give the following reply, which, we doubt not, will rather surprise some of our readers.",
+    "The salt of the ocean varies considerably in different parts.",
+]
+
+b5 = [
+    "Near the equator, the great heat carries up a larger proportion of water by evaporation than in the more temperate regions.",
+    "Thus, as salt is not removed by evaporation, the ocean in the torrid zone is salter than in the temperate or frigid zones.",
+    "The salts of the sea, and other substances contained in it, are conveyed there by the fresh water streams that pour into it from all the continent of the world",
+    "Here, as these substances cannot be evaporated, they would accumulate to such a degree "
+    "as to render the ocean uninhabitable by living creatures."
+    "The operations of the ocean are manifold.",
+    "But we cannot speak of these things without making passing reference to the operations of water, "
+    "as that wonder-working agent of which the ocean constitutes but a part.",
+    "Nothing in this world is ever lost or annihilated.",
+    "As the ocean receives all the water that flows from the land, "
+    "so it returns that water, fresh and pure, in the shape of vapour, to the skies.",
+    "where, in the form of clouds, it is conveyed to those parts of the earth where its presence is most needed. "
+    "After having gladdened the heart of man by driving his mills and causing his food to grow, "
+    "it finds its way again into the sea: and thus the good work goes on with ceaseless regularity.",
+]
+
+# ===== SHORTER INTERVENING CONTEXT ===== #
+
+c1 = ["And the other:"]
+
+
+# define the prompts
+sce1 = {
+    "1": " ".join(a1),
+    "2": " ".join(a2),
+    "3": " ".join(add_sections((a2, a3), skip_last_item=True)),
+    "4": " ".join(add_sections((a2, a3, a4), skip_last_item=True)),
+    "5": " ".join(add_sections((a2, a3, a4, a5), skip_last_item=True)),
+}
+
+# create randomized version of sce1 by permuting prompts on a word level
+l1 = permute_sents(a1)
+l2 = permute_sents(a2)
+l3 = permute_sents(add_sections((a2, a3), skip_last_item=True))
+l4 = permute_sents(add_sections((a2, a3, a4), skip_last_item=True))
+l5 = permute_sents(add_sections((a2, a3, a4, a5), skip_last_item=True))
+
+sce1rnd = {
+    "1": " ".join(l1),
+    "2": " ".join(l2),
+    "3": " ".join(l3),
+    "4": " ".join(l4),
+    "5": " ".join(l5),
+}
+
+# scenario
+sce2 = {
+    "1": " ".join(b1),
+    "2": " ".join(b2),
+    "3": " ".join(add_sections((a2, b3), skip_last_item=False)),
+    "4": " ".join(add_sections((a2, b3, b4), skip_last_item=False)),
+    "5": " ".join(add_sections((a2, b3, b4, b5), skip_last_item=False)),
+}
+
+sce3 = {
+    "1": c1[0],
+}
+
+# control with permuted prompt
+# define the prompts
+sce4 = {
+    "1": " ".join(a1),
+    "2": " ".join(a2),
+    "3": " ".join(add_sections((a2, a3), skip_last_item=True)),
+    "4": " ".join(add_sections((a2, a3, a4), skip_last_item=True)),
+    "5": " ".join(add_sections((a2, a3, a4, a5), skip_last_item=True)),
+}
+
+# control with permuted prompt
+# define the prompts
+sce5 = {
+    "1": " ".join(a1),
+    "2": " ".join(a2),
+    "3": " ".join(add_sections((a2, a3), skip_last_item=True)),
+    "4": " ".join(add_sections((a2, a3, a4), skip_last_item=True)),
+    "5": " ".join(add_sections((a2, a3, a4, a5), skip_last_item=True)),
+}
+
+# scenario
+token_pairs = (
+    ("Mary", "John"),
+    ("her husband", "his wife"),
+    ("her coworkers", "his coworkers"),
+    ("her neighbors", "his neighbors"),
+    (" she ", " he "),
+    ("She ", "He "),
+    (" her ", " him "),
+    ("herself", "himself"),
+)
+
+sce6 = {
+    "1": replace_tokens(" ".join(a1), token_pairs),
+    "2": replace_tokens(" ".join(a2), token_pairs),
+    "3": replace_tokens(
+        " ".join(add_sections((a2, a3), skip_last_item=False)), token_pairs
+    ),
+    "4": replace_tokens(
+        " ".join(add_sections((a2, a3, a4), skip_last_item=False)), token_pairs
+    ),
+    "5": replace_tokens(
+        " ".join(add_sections((a2, a3, a4, a5), skip_last_item=False)), token_pairs
+    ),
+}
+
+# copy sce1 before it has the prompt string added
+sce7 = sce1.copy()
+
+# Join the context strings and the prompt
+prompt_string = "When she got back, she read the list again:"
+for scen in (sce1, sce1rnd, sce2, sce4):
+    for key in scen.keys():
+        scen[key] = scen[key] + " " + prompt_string
+
+# treat sce5 separately and replace final column
+for key in sce5.keys():
+    sce5[key] = sce5[key] + " " + prompt_string.replace(":", ",")
+
+# replace Mary --> John and the pronouns accordingly
+for key in sce6.keys():
+    sce6[key] = sce6[key] + " " + replace_tokens(prompt_string, token_pairs)
+
+# permute the prompt text
+for key in sce7.keys():
+    sce7[key] = sce7[key] + " " + permute_tokens(prompt_string.strip(":"), 324873) + ":"
+
+prompts = {
+    "sce1": sce1,  # intact condition
+    "sce1rnd": sce1rnd,  # scrambled intervening text
+    "sce2": sce2,  # incongruent intervening text
+    "sce3": sce3,  # short intervening text
+    "sce4": sce4,  # permuted preface string
+    "sce5": sce5,  # replacing ":" --> ","
+    "sce6": sce6,  # replace "Mary" --> "John"
+    "sce7": sce7,  # permute the prompt string
+}
+
+if __name__ == "__main__":
+    # save data for supplementary materials
+
+    savedir = '/scratch2/mrenaudin/colorlessgreenRNNs/wm_tests'
+    noun_list_template = ["$W_{1}, W_{2}, ..., W_{N}$\n"]
+
+    format_text = lambda nouns, intervening_texts: [
+        "\n".join(
+            [
+                "$intervening\_text_{}$: ".format(i + 1) + l + " " + nouns[0]
+                for i, l in enumerate(intervening_texts)
+            ]
+        )
+    ]
+
+    # intact scenario
+    preface_string = prefixes["sce1"]["1"]
+    intervening_texts = "\n\n".join(
+        [preface_string]
+        + noun_list_template
+        + format_text(noun_list_template, list(prompts["sce1"].values()))
+    )
+
+    fname = os.path.join(savedir, "intervening_text_intact.tex")
+    print("Writing {}".format(fname))
+    with open(fname, "w") as f:
+        f.writelines(intervening_texts)
+
+    # scrambled scneario
+    preface_string = prefixes["sce1rnd"]["1"]
+    intervening_texts = "\n\n".join(
+        [preface_string]
+        + noun_list_template
+        + format_text(noun_list_template, list(prompts["sce1rnd"].values()))
+    )
+
+    fname = os.path.join(savedir, "intervening_text_scrambled.tex")
+    print("Writing {}".format(fname))
+    with open(fname, "w") as f:
+        f.writelines(intervening_texts)
+
+    # incongruent scneario
+    preface_string = prefixes["sce2"]["1"]
+    intervening_texts = "\n\n".join(
+        [preface_string]
+        + noun_list_template
+        + format_text(noun_list_template, list(prompts["sce2"].values()))
+    )
+
+    fname = os.path.join(savedir, "intervening_text_incongruent.tex")
+    print("Writing {}".format(fname))
+    with open(fname, "w") as f:
+        f.writelines(intervening_texts)
+
+    # short scenario
+    preface_string = prefixes["sce3"]["1"]
+    intervening_texts = "\n\n".join(
+        [preface_string]
+        + noun_list_template
+        + format_text(noun_list_template, list(prompts["sce3"].values()))
+    )
+
+    fname = os.path.join(savedir, "intervening_text_short.tex")
+    print("Writing {}".format(fname))
+    with open(fname, "w") as f:
+        f.writelines(intervening_texts)
+
+    # save as .json files as well
+    fname_prefixes = os.path.join(savedir, "preface_text.json")
+    fname_intervening_texts = os.path.join(savedir, "intervening_texts.json")
+
+    print("Writing {}".format(fname_prefixes))
+    with open(fname_prefixes, "w") as f:
+        json.dump(prefixes, f)
+
+    print("Writing {}".format(fname_intervening_texts))
+    with open(fname_intervening_texts, "w") as f:
+        json.dump(prompts, f)
